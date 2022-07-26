df5854a434836b64ef372728ed80a24f8ce46d37
HAKANKOKCU <103432992+HAKANKOKCU@users.noreply.github.com>
2022-07-26 19:14:13 +0300

    Added App Files

---
+++ Budgie Dock Temp Cleaner/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 94)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(335, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Clean Now"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(311, 26)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "With this tool, You Can Delete Most Of Budgie Dock Temp Files." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "You need to exit " & _
+            "all ""Budgie Dock"" Proceses."
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 70)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(332, 18)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "0 Bytes Cleaned"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(359, 129)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Budgie Dock Temp Cleaner"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Budgie Dock Temp Cleaner/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        BackgroundWorker1.ReportProgress(0)
+        Dim filestonotdelete As New ArrayList
+        For Each ff As String In My.Computer.FileSystem.GetFiles(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\BudgieDock")
+            Dim nm = My.Computer.FileSystem.GetName(ff)
+            If nm.StartsWith("Icon") And nm.EndsWith(".data") Then
+                For Each ic As String In My.Computer.FileSystem.ReadAllText(ff).Split("|")
+                    If ic.Split(":")(0) = "icon" Then
+                        filestonotdelete.Add(ic.Split("*")(1).Replace("{BD-TD-}", ":"))
+                    End If
+                Next
+            End If
+        Next
+        Dim fis = My.Computer.FileSystem.GetFiles(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\BudgieDock\Icons")
+        Dim ci = 0
+        Dim cleanedsize = 0
+        For Each dd As String In fis
+            ci += 1
+            Try
+                If Not filestonotdelete.Contains(dd) Then
+                    Dim x As New IO.FileInfo(dd)
+                    cleanedsize += x.Length
+                    My.Computer.FileSystem.DeleteFile(dd)
+                    BackgroundWorker1.ReportProgress((ci / fis.Count) * 100, cleanedsize.ToString)
+                End If
+            Catch
+            End Try
+        Next
+        BackgroundWorker1.ReportProgress(100, cleanedsize)
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(ByVal sender As System.Object, ByVal e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+        ProgressBar1.Invalidate()
+        Label2.Text = e.UserState & " Bytes Cleaned"
+        Label2.Invalidate()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        BackgroundWorker1.RunWorkerAsync()
+        Button1.Enabled = False
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(ByVal sender As System.Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        Button1.Enabled = True
+    End Sub
+End Class
